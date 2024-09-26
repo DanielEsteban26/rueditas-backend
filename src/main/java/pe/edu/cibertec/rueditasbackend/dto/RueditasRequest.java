e2f86095b1d8eb8b6c9ee36d41b6fb1ba5f9dc30
Daniel Isidro <isidroesteban402@gmail.com>
2024-09-26 11:17:38 -0500

    añadiendo los dto y txt

---
+++ src/main/java/pe/edu/cibertec/rueditasbackend/dto/RueditasRequest.java
@@ -0,0 +1,4 @@
+package pe.edu.cibertec.rueditasbackend.dto;
+
+public record RueditasRequest(String placa) {
+}
---
+++ src/main/java/pe/edu/cibertec/rueditasbackend/service/imp/ServiceRueditas.java
@@ -0,0 +1,9 @@
+package pe.edu.cibertec.rueditasbackend.service.imp;
+
+import pe.edu.cibertec.rueditasbackend.dto.RueditasRequest;
+
+public interface ServiceRueditas {
+    String[] buscarRueditas(RueditasRequest rueditasRequest) throws Exception;
+
+
+}
