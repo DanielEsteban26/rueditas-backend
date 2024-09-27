152a376df166ed134b8c1ef076fcf5044f7d03ab
Daniel Isidro <isidroesteban402@gmail.com>
2024-09-26 20:03:33 -0500

    documentando el codigo y finalizando con el examen

--- src/main/java/pe/edu/cibertec/rueditasbackend/controller/RueditasController.java
+++ src/main/java/pe/edu/cibertec/rueditasbackend/controller/RueditasController.java
@@ -9,7 +9,6 @@ import pe.edu.cibertec.rueditasbackend.dto.RueditasRequest;
 import pe.edu.cibertec.rueditasbackend.dto.RueditasResponse;
 import pe.edu.cibertec.rueditasbackend.service.ServiceRueditas;
 
-
 @RestController
 @RequestMapping("/rueditas")
 public class RueditasController {
@@ -17,19 +16,28 @@ public class RueditasController {
     @Autowired
     ServiceRueditas serviceRueditas;
 
+    /**
+     * Endpoint para buscar información de "rueditas" por placa.
+
+     * rueditasRequest Objeto que contiene la placa a buscar.
+     * return RueditasResponse con la información encontrada o un mensaje de error.
+     */
     @PostMapping("/buscar")
     public RueditasResponse buscarRueditas(@RequestBody RueditasRequest rueditasRequest) {
         try {
+            // Llama al servicio para buscar la información de "rueditas" por placa.
             String[] dates = serviceRueditas.buscarRueditas(rueditasRequest);
+
+            // Si no se encuentra la placa, devuelve una respuesta indicando que no se encontró.
             if (dates == null) {
                 return new RueditasResponse("01", "Placa no Encontrada", null, null, null, null, null);
-            }else {
+            } else {
+                // Si se encuentra la placa, devuelve una respuesta con la información encontrada.
                 return new RueditasResponse("00", "Placa Encontrada", dates[0], dates[1], dates[2], dates[3], dates[4]);
             }
         } catch (Exception e) {
+            // En caso de error, devuelve una respuesta con un mensaje de error.
             return new RueditasResponse("99", "Error 404", null, null, null, null, null);
         }
-
     }
-
 }
--- src/main/java/pe/edu/cibertec/rueditasbackend/service/imp/RueditasServiceImp.java
+++ src/main/java/pe/edu/cibertec/rueditasbackend/service/imp/RueditasServiceImp.java
@@ -11,38 +11,38 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-@Service
+@Service // Anotación que indica que esta clase es un servicio de Spring.
 public class RueditasServiceImp implements ServiceRueditas {
 
     @Autowired
-    ResourceLoader resourceLoader;
+    ResourceLoader resourceLoader; // Inyección de dependencia de ResourceLoader para cargar recursos.
 
     @Override
     public String[] buscarRueditas(RueditasRequest rueditasRequest) throws Exception {
 
-        String[] rueditas = null;
-        Resource resource = resourceLoader.getResource("classpath:rueditas.txt");
+        String[] rueditas = null; // Inicializa el array rueditas como null.
+        Resource resource = resourceLoader.getResource("classpath:rueditas.txt"); // Carga el recurso rueditas.txt desde el classpath.
 
-        try (BufferedReader br = new BufferedReader(new FileReader(resource.getFile()))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] dates = line.split(";");
-                if (rueditasRequest.placa().equals(dates[1])) {
-                    rueditas = new String[5];
+        try (BufferedReader br = new BufferedReader(new FileReader(resource.getFile()))) { // Abre un BufferedReader para leer el archivo rueditas.txt.
+            String line; // Declara una variable line para almacenar cada línea leída del archivo.
+            while ((line = br.readLine()) != null) { // Lee cada línea del archivo hasta que no haya más líneas (null).
+                String[] dates = line.split(";"); // Divide la línea leída en un array de String usando ; como delimitador.
+                if (rueditasRequest.placa().equals(dates[1])) { // Compara la placa del RueditasRequest con el segundo elemento del array dates.
+                    rueditas = new String[5]; // Inicializa el array rueditas con un tamaño de 5.
                     rueditas[0] = dates[2];
                     rueditas[1] = dates[3];
                     rueditas[2] = dates[4];
                     rueditas[3] = dates[5];
-                    rueditas[4] = dates[6];
-                    break;
+                    rueditas[4] = dates[6]; // Asigna los valores correspondientes del array dates al array rueditas.
+                    break; // Sale del bucle while una vez que se encuentra la placa.
                 }
             }
 
         } catch (IOException e) {
-            throw new Exception("Error al leer el archivo de rueditas");
+            throw new Exception("Error al leer el archivo de rueditas"); // Captura cualquier IOException y lanza una nueva excepción con un mensaje de error.
         }
 
-        return rueditas;
+        return rueditas; // Devuelve el array rueditas.
 
     }
 }
